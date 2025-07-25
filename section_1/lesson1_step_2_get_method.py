a99230d1d526b1d4d415a561484d996bd8774196
Ivan Kazakov <vanyakazakov499@gmail.com>
2025-07-25 22:47:25 +0400

    Add lesson6_step4_input_task

--- section_1/lesson1_step_2_get_method.py
+++ section_1/lesson1_step_2_get_method.py
@@ -1,12 +1,13 @@
 import time
 
 # webdriver это и есть набор команд для раоты с браузером
-from selenium import  webdriver
+from selenium import webdriver
+
 # импортируем класс By, который позволяет выбрать способ поиска элемента
 from selenium.webdriver.common.by import By
 
 # инициализируем драйвер браузера. После этой команды вы должны увидеть новое открытое окно браузера
-driver=webdriver.Chrome()
+driver = webdriver.Chrome()
 
 # ожидаем 5 секунд, чтобы мы могли увидеть, что происходит в браузере
 time.sleep(5)
@@ -18,14 +19,14 @@ time.sleep(5)
 # Метод find_element позволяет найти нужный элемент на сайте, указав путь к нему.
 # Метод принимает в качестве аргументов способ поиска и значение, по которому мы будем искать.
 # Ищем поле для ввода текста
-textarea=driver.find_element(By.CSS_SELECTOR, ".textarea")
+textarea = driver.find_element(By.CSS_SELECTOR, ".textarea")
 
 # Напишем текст ответа в найденное поле
 textarea.send_keys("get()")
 time.sleep(5)
 
 # Найдем кнопку, которая отправляет введенное решение
-submit_button=driver.find_element(By.CSS_SELECTOR, ".submit-submission")
+submit_button = driver.find_element(By.CSS_SELECTOR, ".submit-submission")
 
 # Сообщаем драйверу, что нужно нажать на кнопку. После этой команды мы должны увидеть сообщение о правильном ответе
 submit_button.click()
